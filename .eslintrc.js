ca1699660ed00778c707edb5296e3d9657668f64
尤金金 <jinjin.you@baozun.com>
2020-12-22 10:06:55 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
+    'vue/no-side-effects-in-computed-properties': 0,
+    'no-extend-native': 0,
+    eqeqeq: 'off',
+    'space-before-function-paren': 0
+  }
+}
